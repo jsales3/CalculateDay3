0275ffa46332b59d5acfdaf91d49cc8c0d68a8b5
jsales3 <jovonsales@gmail.com>
2018-03-03 15:12:11 -0500

    first commit

---
+++ CalculateDay3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateDay3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jovon Sales");
+            Console.WriteLine("jsales3@kent.edu");
+
+            DateTime myValue = DateTime.Now;
+
+            Console.WriteLine("Enter your Birthdate in mm/dd/yyyy format:");
+
+            string userValue = Console.ReadLine();
+
+            while (true)
+            {
+
+                try
+                {
+
+                    DateTime myBirthday = DateTime.Parse(userValue);
+                    TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+                    Console.WriteLine(myAge.TotalDays);
+
+                    Console.ReadLine();
+                    break;
+                }
+
+                catch (Exception) { }
+                Console.WriteLine("The date you entered is incorrect");
+                Console.WriteLine("Please enter a correct date");
+                userValue = Console.ReadLine();
+            }
+        }
+    }
+}
